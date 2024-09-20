f7d4499ed7e1de8c437d9b3b86c79ce67de940fb
jungseokman <seokman.jung@gmail.com>
2024-09-20 14:52:14 +0900

    basic

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Dream Coding in Docker"));
+
+app.listen(8080, () => console.log("Server is running"));
